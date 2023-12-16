6c7a09b18bd5a7c8a1650cdeb84d34da506a7654
Phillip Bailey <ztron500@gmail.com>
2023-12-15 20:04:00 -0500

    update

--- app/pages/Contact/ContactForm.jsx
+++ app/pages/Contact/ContactForm.jsx
@@ -1,192 +1,179 @@
 'use client'
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import contactimg from '../../img/contact-itruthnews.png';
 import { BeatLoader } from 'react-spinners';
+import { addDoc, collection, getDoc, getFirestore, doc } from 'firebase/firestore';
+import { auth, db } from '@/app/Config/firebase';
 import { getAuth } from 'firebase/auth';
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '@/app/Config/firebase';
-
-
+import ContactModal from './contactModal';
 
 export default function ContactForm() {
+const [names, setNames] = useState('');
+const [ email, setEmail ] = useState('');
+const [ subject, setSubject ] = useState('');
+const [contact, setContact] = useState("");
+const [content, setContent] = useState("");
 const [isLoading, setIsLoading] = useState(false);
 const [isSignedIn, setIsSignedIn] = useState(false);
-
-const [user, setUser] = useState('');
-const [error, setError] = useState('');
+const [errorMessage, setErrorMessage] = useState('');
 const [successMessage, setSuccessMessage] = useState('');
-const [fname, setFname] = useState();
-const [subject, setSubject] = useState();
-const [email, setEmail] = useState();
-const [content, setContent] = useState();
+const [autoFocus, setAutoFocus] = useState(true);
+const [editModalOpen, setEditModalOpen] = useState(false);
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      const getUserData = async (userId) => {
+        try {
+          const db = getFirestore();
+          const userDocRef = doc(db, 'users', userId);
+          const userDocSnapshot = await getDoc(userDocRef);
+          if (userDocSnapshot.exists()) {
+            const userData = userDocSnapshot.data();
+            return userData;
+          } else {
+            return null;
+          }
+        } catch (error) {
+          throw error;
+        }
+      };
+  
+      setIsSignedIn(!!user);
+  
+      if (user) {
+        try {
+          const userData = await getUserData(user.uid);
+          setNames(userData.names || ''); // Set names to userData.names or an empty string
+        } catch (error) {
+          setErrorMessage(error);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    });
+  
+    return () => unsubscribe();
+  }, []);
 
-  try {
-    setIsLoading(true);
-
-    const auth = getAuth();
-    const user = auth.currentUser;
-    const db = getFirestore();
- 
-
-    if (userDocSnapshot.exists()) {
-      const userData = userDocSnapshot.data();
-      // Do something with userData if needed
-    } else {
-      // Handle the case when the document doesn't exist
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const user = auth.currentUser;
+      setIsLoading(true);
+      const db = getFirestore();
+      const docRef = await addDoc(collection(db, 'contacts'), {
+        content: content,
+        timestamp: new Date(),
+        userName: user.displayName,
+        userEmail: user.email,
+      });
+      setContact((prevComments) => [
+        ...prevComments,
+        {
+          id: docRef.id,
+          content: content,
+          timestamp: new Date(),
+          userName: user.displayName,
+          userEmail: user.email,
+        },
+      ]);
+  
+      setEditModalOpen(true);
+  
+      setTimeout(() => {
+        setEditModalOpen(false);
+      }, 5000);
+  
+      setSuccessMessage('Comment created successfully');
+      setTimeout(() => {
+        setSuccessMessage('');
+      }, 3000);
+  
+      setNames('');
+      setEmail('');
+      setSubject('');
+      setContent('');
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      setErrorMessage('Error submitting form. Please try again.');
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 3000);
+    } finally {
+      setIsLoading(false);
     }
+  };
+  
 
-    setSuccessMessage('Form submitted successfully');
-  } catch (error) {
-    // Handle submission error, update error state
-    setError('An error occurred during form submission');
-  } finally {
-    setIsLoading(false);
-  }
-};
+  
 
-      
-    
-const handleChange = (e) => {
-  const { name, value } = e.target;
-  setContent((prevValues) => ({
-    ...prevValues,
-    [name]: value,
-  }));
+  return (
+    <>
+    <ContactModal  />
+    {editModalOpen && <ContactModal  />}
 
-  // Additional handling for specific fields (subject, message, email)
-  if (name === 'subject') {
-    // Additional logic for handling the 'subject' field
-  } else if (name === 'message') {
-    // Additional logic for handling the 'message' field
-  } else if (name === 'email') {
-    // Additional logic for handling the 'email' field
-  }
-};
+      <div className='contact_title_img'>
+        <Link href='/'>
+          <Image title='Home Page' height={34} src={contactimg} alt='...' />
+        </Link>
+      </div>
+      <div style={{ display: 'grid', placeContent: 'center', maxWidth: '30rem', margin: 'auto' }}>
+        <form className='formbox' onSubmit={handleSubmit}>
+          <label htmlFor='fname' aria-label="Name">Name</label>
+          <input
+            type="text"
+            name="fname"
+            value={names} onChange={(e) => setNames(e.target.value)} required
+          />
 
-    
-const onBlur = (e) => {
-  const { name, value } = e.target;
+          <label htmlFor='email' aria-label="Email">Email</label>
+          <input
+            type="email"
+            name="email"
+            aria-describedby="emailError"
+            value={email}
+onChange={(e) => {
+setEmail(e.target.value);
 
-  // Example: Perform validation based on the field name
-  switch (name) {
-    case 'name':
-      // Example: Validate that the name is not empty
-      if (!value.trim()) {
-        setError('Name is required');
-      } else {
-        setError('');
-      }
-      break;
+}}
+          />
 
-    case 'email':
-      // Example: Validate email format
-      const isValidEmail = validateEmail(value);
-      if (!isValidEmail) {
-        setError('Invalid email format');
-      } else {
-        setError('');
-      }
-      break;
+          <label htmlFor='subject' aria-label="Subject">Subject</label>
+          <input
+          type="text"
+          name="subject"
+          aria-describedby="subjectError"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)} // Assuming you want to capture the subject
+        />
 
-    case 'subject':
-      // Example: Validate that the subject is not empty
-      if (!value.trim()) {
-        setError('Subject is required');
-      } else {
-        setError('');
-      }
-      break;
+          <label htmlFor='content' aria-label="Type Your Message">Type Your Message</label>
+          <textarea
+            type="text"
+            name="content"
+            rows={5}
+            required
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            autoFocus={autoFocus}
+            aria-describedby="messageError"
+          />
 
-    case 'message':
-      // Example: Validate that the message is not empty
-      if (!value.trim()) {
-        setError('Message is required');
-      } else {
-        setError('');
-      }
-      break;
+          <button
+            className={isSignedIn ? "submitbtn" : "submitbtn disabled"}
+            type="submit"
+            disabled={!isSignedIn || !content || isLoading}>
+            {isLoading ? <BeatLoader color='blue' /> : 'Comment'}
+          </button>
 
-    // Add more cases for other fields as needed
+        </form>
 
-    default:
-      // Handle other fields if necessary
-  }
-};
-// Example validation function for email
-const validateEmail = (email) => {
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-return emailRegex.test(email);
-};
-return (
-<>
-<div className='contact_title_img'>
-<Link href='/'>
-<Image title='Home Page' height={34} src={contactimg} alt='...' />
-</Link>
-</div>
-<div style={{ display: 'grid', placeContent: 'center', maxWidth:'30rem', margin: 'auto' }}>
-<form className='formbox' onSubmit={handleSubmit}>
-  <label htmlFor='fname' aria-label="Name">Name</label>
-  <input
-    type="text"
-    name="name"
-    id='fname'
-    value={fname}
-    onChange={handleChange}
-  />
-
-  <label htmlFor='email' aria-label="Email">Email</label>
-  <input
-    id='email'
-    type="email"
-    name="email" 
-    value={email}
-    onChange={handleChange}
-    onBlur={onBlur}
-    aria-describedby="emailError"
-  />
-
-  <label htmlFor='subject' aria-label="Subject">Subject</label>
-  <input
-    id='subject'
-    type="text"
-    name="subject"
-    value={subject}
-    onChange={handleChange}
-    onBlur={onBlur}
-    aria-describedby="subjectError"
-  />
-
-  <label htmlFor='message' aria-label="Type Your Message">Type Your Message</label>
-  <textarea
-    type="text"
-    id='message'
-    name="message"
-    rows={5}
-    value={content}
-    onChange={handleChange}
-    onBlur={onBlur}
-    aria-describedby="messageError"
-  />
-
-<button
-  className={isSignedIn ? "submitbtn" : "submitbtn disabled"}
-  type="submit"
-  disabled={!isSignedIn || !content || isLoading}
->
-  {isLoading ? <BeatLoader color='blue' /> : 'Comment'}
-</button>
-
-</form>
-
-{error && <p id="emailError" style={{ color: 'red', textAlign: 'center' }}>{error}</p>}
-{successMessage && (<p id="successMessage" style={{ color: "green", textAlign: "center" }}>{successMessage}</p>)}
-</div>
-</>
-);
+        {errorMessage && <p className='error'>{errorMessage.toString()}</p>}
+        {successMessage && <p className="success">{successMessage}</p>}
+      </div>
+    </>
+  );
 }
